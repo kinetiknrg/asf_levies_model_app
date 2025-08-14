b0b3ace135c3b069f63e09b38dccbf31b63386c3
kinetiknrg <richard@kinetik-nrg.com>
2025-08-14 02:08:26 +0100

    fixing emoji in filename issue related to Streamlit deployment

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ It imports and runs the main application file from within the package,
 ensuring that Python's import system correctly recognizes it as a package.
 """
 
-from asf_levies_model_app import 🎯_Run_a_rebalancing_scenario
+from asf_levies_model_app import run_rebalancing_scenario
